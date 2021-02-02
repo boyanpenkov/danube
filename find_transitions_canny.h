0236bca53c8c0e7d9f3f7ef55770848e4e484fab
Boyan Penkov <boyan.penkov@gmail.com>
2021-02-01 19:35:10 -0500

    used clang-format

--- find_transitions_canny.h
+++ find_transitions_canny.h
@@ -1,51 +1,47 @@
-__global__ void find_transitions_canny(float *d_v,
-					   int *d_trans,
-					   int d_ppt,
-					   int *d_s,
-					   float *d_grad
-					   )
-{  __shared__ int found[MACRO_THREADS];
+__global__ void find_transitions_canny(float *d_v, int *d_trans, int d_ppt,
+                                       int *d_s, float *d_grad) {
+  __shared__ int found[MACRO_THREADS];
 
-  //Initialize the array
+  // Initialize the array
   for (int i = 0; i < blockDim.x; i++)
     found[i] = 0;
 
   float EVENT_THRESHOLD = 1;
-  //Tunable -- edge cutoff
-  
-  int idx = blockIdx.x*blockDim.x*(d_ppt) + threadIdx.x*(d_ppt);
-  
-  for (int i = idx; i < idx + (d_ppt); i++){
+  // Tunable -- edge cutoff
+
+  int idx = blockIdx.x * blockDim.x * (d_ppt) + threadIdx.x * (d_ppt);
+
+  for (int i = idx; i < idx + (d_ppt); i++) {
     if (i == 0)
       d_grad[i] = (d_v[i + 1] - d_v[i]) / 2;
     else if (i >= *d_s - 1)
       d_grad[*d_s - 1] = (d_v[*d_s - 1] - d_v[*d_s - 2]) / 2;
-    else{
+    else {
       d_grad[i] = fabsf((d_v[i + 1] - d_v[i - 1]) / 2);
-      if (d_grad[i] > EVENT_THRESHOLD)
-	{found[threadIdx.x] = 1;
-	  d_grad[i] = d_v[i];
-	}
-      else d_grad[i] = NOT_EVENT;
+      if (d_grad[i] > EVENT_THRESHOLD) {
+        found[threadIdx.x] = 1;
+        d_grad[i] = d_v[i];
+      } else
+        d_grad[i] = NOT_EVENT;
     }
   }
-  
+
   __syncthreads();
-  
-  if (threadIdx.x == 0){
+
+  if (threadIdx.x == 0) {
     int block_trans = 0;
     if (found[0] == 1)
       block_trans++;
     // Avoid double counting of the same transition
     for (int i = 1; i < blockDim.x; i++)
       if (found[i] == 1 && found[i - 1] == 0)
-	block_trans++;
+        block_trans++;
     d_trans[blockIdx.x] = block_trans;
-    
+
     // Discard edges
     for (int i = 0; i < 50; i++)
-      d_grad[i]  = 0;
-    for (int i = gridDim.x * blockDim.x * (d_ppt) - 1; i < (*d_s); i++)
+      d_grad[i] = 0;
+    for (int i = gridDim.x * blockDim.x * (d_ppt)-1; i < (*d_s); i++)
       d_grad[i] = 0;
   }
 }
--- mean_filter_signal.h
+++ mean_filter_signal.h
@@ -1,24 +1,19 @@
-__global__ void mean_filter_signal(float *d_values,
-				   int d_ppt,
-				   int window_width,
-				   int *d_size,
-				   float *d_smoothed
-				   )
-{
-  int idx = blockIdx.x * blockDim.x * d_ppt + threadIdx.x*d_ppt;
+__global__ void mean_filter_signal(float *d_values, int d_ppt, int window_width,
+                                   int *d_size, float *d_smoothed) {
+  int idx = blockIdx.x * blockDim.x * d_ppt + threadIdx.x * d_ppt;
   for (int i = idx; i < idx + d_ppt; i++) {
     float sum = 0;
     for (int j = 0; j <= window_width / 2; j++) {
       if (i - j < 0)
-	sum += d_values[i];
+        sum += d_values[i];
       else
-	sum += d_values[i - j];
+        sum += d_values[i - j];
     }
     for (int j = 1; j <= window_width / 2; j++) {
-      if (i + j >= *d_size){
-	sum += d_values[i];}
-      else
-	sum += d_values[i + j];
+      if (i + j >= *d_size) {
+        sum += d_values[i];
+      } else
+        sum += d_values[i + j];
     }
     sum /= window_width;
     d_smoothed[i] = sum;
